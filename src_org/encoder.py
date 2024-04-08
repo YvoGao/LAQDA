a4a671aebc893941d15cc35ee2e0692c799728af
YvoGao <58680618+YvoGao@users.noreply.github.com>
2024-04-08 22:18:22 +0800

    Update encoder.py

--- src_org/encoder.py
+++ src_org/encoder.py
@@ -35,7 +35,11 @@ class BertEncoder(nn.Module):
         self.tokenizer = BertTokenizer.from_pretrained(args.fileVocab, do_lower_case=True)
         config = BertConfig.from_json_file(args.fileModelConfig)   
         self.bert = BertModel.from_pretrained(args.fileModel, config=config)
-        self.template = nn.Parameter(torch.randn([1,768]))
+        self.attention = copy.deepcopy(self.bert.encoder.layer[11])
+        self.lin = nn.Linear(768, 768)
+        self.drop = nn.Dropout(0.1)
+        self.la = args.la
+        # self.att = AttentionMapper(768,768,2)
         if args.numFreeze > 0:
             self.freeze_layers(args.numFreeze)
 
@@ -53,11 +57,6 @@ class BertEncoder(nn.Module):
                     break
     
     def forward(self, text, task_classes):
-        # # 使用模板
-        # template = 'Topic is [MASK]:' 
-        # for il in label:
-        #     template += il
-        # sentence = [template + x for x in text]
         sentence = [x for x in text]
         tokenizer = self.tokenizer(
         sentence,
@@ -70,52 +69,23 @@ class BertEncoder(nn.Module):
         token_type_ids = tokenizer['token_type_ids'].cuda()
         attention_mask = tokenizer['attention_mask'].cuda()
 
-        # org
+
+        
         outputs = self.bert(
               input_ids,
               attention_mask=attention_mask,
               token_type_ids=token_type_ids
               )
-        ## mean
-        # return outputs.last_hidden_state.mean(dim=1)
+        if self.la == 0:
+            return outputs.last_hidden_state.mean(dim=1)
 
-        # # 增加标签到句子前
-        # alllabel = task_classes[0] + " " + task_classes[1] + " " + task_classes[2] + " " + task_classes[3] + " " + task_classes[4]
-        # label_texts = [ alllabel for i in text]
-        
-        # task_labels_inputs = self.tokenizer.batch_encode_plus(
-        #     label_texts, return_tensors='pt', padding=True)
-        # task_labels_inputs_input_ids = task_labels_inputs['input_ids'].cuda()
-        # task_labels_inputs_token_type_ids = task_labels_inputs['token_type_ids'].cuda()
-        # task_labels_inputs_attention_mask = task_labels_inputs['attention_mask'].cuda()
-        
-        # input_ids = torch.cat(
-        #     (task_labels_inputs_input_ids, input_ids[:, 1:]), dim=1)
-        # token_type_ids = torch.cat(
-        #     (task_labels_inputs_attention_mask, token_type_ids[:, 1:]), dim=1)
-        # attention_mask = torch.cat(
-        #     (task_labels_inputs_token_type_ids, attention_mask), dim=1)
 
-        # sentence_embeddings = self.bert.embeddings(input_ids, token_type_ids)
-        
-        ## 软标签
-        # predfix = self.template.unsqueeze(dim=0).repeat_interleave(sentence_embeddings.shape[0], dim=0)
-        # # import pdb
-        # # pdb.set_trace()
-        # connect_attention = self.bert.get_extended_attention_mask(attention_mask, input_ids.size())
-        # connect_embeddings = torch.cat((sentence_embeddings[:,0,:].unsqueeze(dim=1), predfix, sentence_embeddings[:,1:,:]), dim=1)
-        
-        # outputs = self.bert.encoder(connect_embeddings, attention_mask=connect_attention)
-        # outputs = self.bert.pooler(outputs.last_hidden_state)
-        
-        # return outputs
-        # add attention
+        # add
         task_labels_inputs = self.tokenizer.batch_encode_plus(
             task_classes, return_tensors='pt', padding=True)
         task_labels_inputs_input_ids = task_labels_inputs['input_ids'].cuda()
         task_labels_inputs_token_type_ids = task_labels_inputs['token_type_ids'].cuda()
         task_labels_inputs_attention_mask = task_labels_inputs['attention_mask'].cuda()
-
 
         label_embedding = self.bert(
             task_labels_inputs_input_ids,
@@ -123,44 +93,21 @@ class BertEncoder(nn.Module):
             task_labels_inputs_token_type_ids
             ).last_hidden_state.mean(dim=1)
         
+       
 
-        # import pdb
-        # pdb.set_trace()
         # add
         sentence_embeddings = outputs.last_hidden_state
         label_embeddings =  label_embedding.unsqueeze(dim=0).repeat_interleave(sentence_embeddings.shape[0], dim=0)
         connect_embeddings = torch.cat((sentence_embeddings, label_embeddings), dim=1)
         outputs = self.lin(connect_embeddings)
         outputs = self.drop(outputs)
-        outputs = 0.9* self.attention(connect_embeddings)[0] + 0.1 * sentence_embeddings
+        outputs = self.attention(connect_embeddings)[0]
+        outputs = 0.1* outputs + 0.9* connect_embeddings
         
-        return outputs[:,0,:]
+        outputs = outputs[:,0,:]
+        return outputs
         
-        # predictions = torch.zeros(input_ids.shape[0], outputs.last_hidden_state.shape[2]).cuda()
-        # mask_token_index = torch.where(input_ids == self.tokenizer.mask_token_id)[1]
-        # for i in range(len(predictions)):
-        #     predictions[i] = outputs.last_hidden_state[i,mask_token_index[i],:]
-        # return predictions
-
-
-# class Buffer(nn.Module):
-#     def __init__(self, args):
-#         super(Buffer, self).__init__()
-#         self.memory = {}
-#         self.memory_size = {}
-#         self.classes = args.classes
-#         self.init_memory()
-
-#     def init_memory(self):
-#         for idx in self.classes:
-#             self.memory_size[idx] = 0
-#             self.memory[idx] = np.random.randn(
-#                 self.num_centroids, self.embedding_size)
-
-
-
-
-
+        
 class Sampler(nn.Module):
     def __init__(self, args):
         super(Sampler, self).__init__()
@@ -188,6 +135,7 @@ class Sampler(nn.Module):
     def forward(self, support_embddings, query_embeddings):
         # (NK, NQ)
         similarity = euclidean_dist(support_embddings, query_embeddings)
+        similarity =similarity.view(self.nway, 1, -1)
         # (N, K, NQ)
         similarity = similarity.view(self.nway, self.kshot, -1)
       
@@ -212,27 +160,6 @@ class Sampler(nn.Module):
         for i in range(nindices.shape[0]):
             convex_feat.append(query_embeddings.index_select(0, nindices[i]))
         convex_feat = torch.stack(convex_feat) # NK, k, 768
-
-        # # 计算分布采样
-        # calculate variance
-        # convex_mean, convex_cov = self.calculate_var(convex_feat)
-        # n_lsamples = support_embddings.shape[0]
-
-        # sampled_data = []
-        # for i in range(n_lsamples):
-        #     mean, cov = self.distribution_calibration(support_embddings[i], convex_mean[i], convex_cov[i])
-           
-        #     samples = []
-        #     for i in range(mean.shape[0]):
-        #         samples.append(torch.normal(mean[i].cpu().detach(), cov[i].cpu().detach(), size=(1, self.num_sampled)))
-        #     samples = torch.stack(samples)
-            
-        #     samples = samples.squeeze().permute(1, 0)
-        #     sampled_data.append(samples)
-           
-        # NK, num_sampled, 768 -> N, K*num_smpled, 768
-        # sampled_data = torch.stack(sampled_data).view(self.nway, self.kshot*self.num_sampled, self.dim)
-
         sampled_data = convex_feat.view(self.nway, self.kshot*self.k, self.dim)
        
         return sampled_data, acc
@@ -253,22 +180,7 @@ class MyModel(nn.Module):
         self.sampler = Sampler(args)
         # self.reference = nn.Linear(768, self.args.numNWay, bias=True)
 
-    def Transformation_Matrix(self, prototype):
-        C = prototype # (N, emd_dim)
-        eps = 1e-6 #避免除以0的情况
-        R = self.reference.weight # (emd_dim, N)
-
-        # 标准化
-        power_R = ((R * R).sum(dim=1, keepdim=True)).sqrt()
-        R = R / (power_R + eps)
-
-        # 标准化
-        power_C = ((C * C).sum(dim=1, keepdim=True)).sqrt()
-        C = C / (power_C + eps)
-
-        P = torch.matmul(torch.pinverse(C), R)
-        P = P.permute(1, 0)
-        return P
+    
     
     def forward(self, text, label):
         support_size = self.args.numNWay * self.args.numKShot
@@ -278,33 +190,23 @@ class MyModel(nn.Module):
         support_embddings = text_embedding[:support_size]  # NK X 768
         query_embeddings = text_embedding[support_size:]   # NQ X 768
         
-        # N, S, dim
-        sampled_data, acc = self.sampler(support_embddings, query_embeddings)
-        # sampled_data = sampled_data.cuda()
         
-        
-        # 不进行分布矫正
-        # sampled_data = support_embddings.view(self.args.numNWay, -1, support_embddings.shape[1])
-        # acc = 0
-        # prototypes = c_prototypes.mean(dim=1)
-
         # calculate prototypes
         c_prototypes = support_embddings.view(self.args.numNWay, -1, support_embddings.shape[1])  # N X K X dim
+        # calculate original prototypes for generating loss
+        original_prototypes = c_prototypes.mean(dim=1)
+
+        # N, S, dim
+        sampled_data, acc = self.sampler(support_embddings, query_embeddings)
+
         prototypes = torch.cat((c_prototypes, sampled_data), dim=1)
         prototypes = torch.mean(prototypes, dim=1)
 
-        # calculate original prototypes for generating loss
-        original_prototypes = c_prototypes.mean(dim=1)
-        
-        # 装换
-        # P = self.Transformation_Matrix(prototypes)
-        # weight = P.view(P.size(0), P.size(1), 1)
-        # prototypes = F.conv1d(prototypes.squeeze(0).unsqueeze(2), weight).squeeze(2)
-        # query_embeddings = F.conv1d(query_embeddings.squeeze(0).unsqueeze(2), weight).squeeze(2)
         
         return (prototypes, query_embeddings, acc, original_prototypes, sampled_data)
 
 
+    
     def visual(self, text):
         support_size = self.args.numNWay * self.args.numKShot
         query_size = self.args.numNWay * self.args.numQShot
@@ -325,3 +227,5 @@ class MyModel(nn.Module):
 
         
         return support_embddings, query_embeddings, sampled_data, prototypes
+    
+
